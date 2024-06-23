dead79b62378ec5fba58a0713c264584dbcc9e9d
Keyur Bhalodiya | KB <kbit920@outlook.com>
2024-06-23 22:00:02 +0530

    Initial Commit

---
+++ GitHubUsers/GitHubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersApp.swift
+//  GitHubUsers
+//
+//  Created by Keyur Bhalodiya on 2024/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
