d679e676ee42f490ebe813c5d9b4e9e8c780e977
Keyur Bhalodiya | KB <kbit920@outlook.com>
2024-06-27 11:44:37 +0530

    * write a unit test for search implementation * add sample JSON file for search result response

--- GitHubUsersTests/MockUserListViewDataProvider.swift
+++ GitHubUsersTests/MockUserListViewDataProvider.swift
@@ -13,18 +13,28 @@ import Combine
 final class MockUserListViewDataProvider: UserListViewDataProviding {
   
   @Published private var users: [User] = []
+  @Published private var usersSearch: [User] = []
 
   var usersPublisher: AnyPublisher<[GitHubUsers.User], Never> {
     $users.eraseToAnyPublisher()
   }
   
+  var usersSearchPublisher: AnyPublisher<[GitHubUsers.User], Never> {
+    $usersSearch.eraseToAnyPublisher()
+  }
+
   var responseToReturn: [User]?
   var errorToReturn: NetworkError?
+  var responseToReturnSearch: UsersSearch?
   
   var hasLoadedAllUsers: Bool = false
   
   func loadGitHubUsers(index: Int?) {
     guard let responseToReturn else { return }
     users = responseToReturn
+  }
+  
+  func searchUsers(with name: String) {
+    usersSearch = responseToReturnSearch?.users ?? []
   }
 }
--- GitHubUsersTests/UserListViewModelTest.swift
+++ GitHubUsersTests/UserListViewModelTest.swift
@@ -45,12 +45,12 @@ final class UserListViewModelTest: AsyncSpec {
     }
     
     describe("loadGitHubUsers with pagination") {
+      beforeEach {
+        mockDataProvider.responseToReturn = Helper.loadLocalTestDataWithoutParsing("UserList", type: [User].self)
+        viewModel = UserListViewModel(dataProvider: mockDataProvider)
+      }
+      
       context("loadGitHubUsers success") {
-        beforeEach {
-          mockDataProvider.responseToReturn = Helper.loadLocalTestDataWithoutParsing("UserList", type: [User].self)
-          viewModel = UserListViewModel(dataProvider: mockDataProvider)
-        }
-        
         it("should return correct users count 10 eventually") {
           await expect(viewModel.users.count).toEventually(equal(5))
           
@@ -60,11 +60,6 @@ final class UserListViewModelTest: AsyncSpec {
       }
       
       context("loadGitHubUsers failed") {
-        beforeEach {
-          mockDataProvider.responseToReturn = Helper.loadLocalTestDataWithoutParsing("UserList", type: [User].self)
-          viewModel = UserListViewModel(dataProvider: mockDataProvider)
-        }
-        
         it("should return users count 5 eventually") {
           await expect(viewModel.users.count).toEventually(equal(5))
           
@@ -72,6 +67,73 @@ final class UserListViewModelTest: AsyncSpec {
           mockDataProvider.errorToReturn = .unknown
           await expect(viewModel.users.count).toEventually(equal(5))
         }
+      }
+    }
+    
+    describe("searchUsers action") {
+      beforeEach {
+        mockDataProvider.responseToReturn = Helper.loadLocalTestDataWithoutParsing("UserList", type: [User].self)
+        viewModel = UserListViewModel(dataProvider: mockDataProvider)
+      }
+      
+      context("searchUsers success") {
+        it("should return search users count 2 eventually") {
+          await expect(viewModel.users.count).toEventually(equal(5))
+          
+          mockDataProvider.responseToReturnSearch = Helper.loadLocalTestDataWithoutParsing("UserSearch", type: UsersSearch.self)
+          viewModel.searchUsers(with: "keyur")
+          await expect(viewModel.users.count).toEventually(equal(2))
+        }
+      }
+      
+      context("searchUsers failed") {
+        it("should return search users count 0 eventually") {
+          await expect(viewModel.users.count).toEventually(equal(5))
+          
+          mockDataProvider.responseToReturnSearch = Helper.loadLocalTestDataWithoutParsing("APIError", type: UsersSearch.self)
+          mockDataProvider.errorToReturn = .unknown
+          viewModel.searchUsers(with: "keyur")
+          await expect(viewModel.users.count).toEventually(equal(0))
+        }
+      }
+          
+      context("userList pagination when isSearching true") {
+        it("shouldn't call loadGitHubUsers and return same count") {
+          await expect(viewModel.users.count).toEventually(equal(5))
+          
+          mockDataProvider.responseToReturnSearch = Helper.loadLocalTestDataWithoutParsing("UserSearch", type: UsersSearch.self)
+          viewModel.searchUsers(with: "keyur")
+
+          viewModel.loadGitHubUsers()
+          await expect(viewModel.users.count).toEventually(equal(5))
+        }
+      }
+    }
+    
+    describe("dismissSearchUsers action") {
+      beforeEach {
+        mockDataProvider.responseToReturn = Helper.loadLocalTestDataWithoutParsing("UserList", type: [User].self)
+        viewModel = UserListViewModel(dataProvider: mockDataProvider)
+      }
+      it("should return users count same as initial value") {
+        await expect(viewModel.users.count).toEventually(equal(5))
+        
+        mockDataProvider.responseToReturnSearch = Helper.loadLocalTestDataWithoutParsing("UserSearch", type: UsersSearch.self)
+        viewModel.searchUsers(with: "keyur")
+        await expect(viewModel.users.count).toEventually(equal(2))
+        
+        viewModel.dismissSearchUsers()
+        await expect(viewModel.users.count).toEventually(equal(5))
+      }
+    }
+    
+    describe("lastUserId") {
+      beforeEach {
+        mockDataProvider.responseToReturn = Helper.loadLocalTestDataWithoutParsing("UserList", type: [User].self)
+        viewModel = UserListViewModel(dataProvider: mockDataProvider)
+      }
+      it("should return correct value") {
+        await expect(viewModel.lastUserId).toEventually(equal(54859))
       }
     }
   }
